1150e2de1a2551fabb572bfb9d185458575ee7fa
ersteller <siggi2017@freenet.de>
2016-05-07 03:07:16 +0200

    removed pickling

--- tickimage.py
+++ tickimage.py
@@ -66,195 +66,160 @@ def main():
     pipperseclastprice = None
     pippersec_t_v = []
     
-    try:
-        pfile = open("EURUSD q.pickle",'r')
-        
-        (
-                    x,
-                    ybid,
-                    yask,
-                    sma_fast_data,
-                    sma_ff_data,
-                    bollinger_data,
-                    marker_data,
-                    rsi_data,
-                    rsi_avg_data,
-                    pippersec_t_v,
-                    accdata
-        ) = cPickle.load(pfile)
-    except Exception as e:
-        #raise e #forbid recalculation
-        pfile = open("EURUSDq.pickle",'w')
-        
-        for l in f.readlines():
-            cnt += 1
-            ##skip until this tick 
-            #if cnt < 100000:
-            #    if not cnt%1000:
-            #        print cnt
-            #    continue
-            ##stop after this tick 
-            #if cnt >= 110000:
-            #    break
 
-            #name datetime bid ask
-            s = l.split(",")
-                
-            datetime, millis = s[1].split('.')
-            sec = calendar.timegm(time.strptime(datetime, "%Y%m%d %H:%M:%S"))
-            secm = sec + int(millis) / 1000.
-            
-            bid, ask = float(s[2]), float(s[3])
+    pfile = open("EURUSDq.pickle",'w')
+    
+    for l in f.readlines():
+        cnt += 1
+        ##skip until this tick 
+        #if cnt < 100000:
+        #    if not cnt%1000:
+        #        print cnt
+        #    continue
+        ##stop after this tick 
+        #if cnt >= 110000:
+        #    break
 
-            if not cnt%1000:
-                print cnt, datetime
+        #name datetime bid ask
+        s = l.split(",")
             
-     
-            x.append(secm)
-            ybid.append(bid)
-            yask.append(ask)
+        datetime, millis = s[1].split('.')
+        sec = calendar.timegm(time.strptime(datetime, "%Y%m%d %H:%M:%S"))
+        secm = sec + int(millis) / 1000.
+        
+        bid, ask = float(s[2]), float(s[3])
+
+        if not cnt%1000:
+            print cnt, datetime
+        
+ 
+        x.append(secm)
+        ybid.append(bid)
+        yask.append(ask)
+        
+        if len(ybid)>= 2:
+            tl = x[-2]
+            t  = x[-1]
+            dt.append(t-tl)
+        else:
+            dt.append(None)
+        
+        sma_fast_data.append(sma_fast.update(dt[-1], bid))
+        sma_ff_data.append(sma_ff.update(dt[-1],bid))
+        bollinger_data.append(bollinger.update(dt[-1], bid))            
+        rsi_data.append(rsi.update(secm, bid))
+        rsi_avg_data.append(rsi_avg.update(dt[-1], rsi_data[-1]))
+        
+        
+        if pipperseclaststamp + 1 <= secm:
+            if pipperseclastprice != None:                    
+                pricediff = bollinger_data[-1][0] - pipperseclastprice 
+                duration = (secm - pipperseclaststamp)
+                if duration != 0:
+                    pippersec = pricediff/duration
+                    pippersec_t_v.append((secm, pippersec))
+            pipperseclaststamp = secm
+            pipperseclastprice = bollinger_data[-1][0]
             
-            if len(ybid)>= 2:
-                tl = x[-2]
-                t  = x[-1]
-                dt.append(t-tl)
-            else:
-                dt.append(None)
-            
-            sma_fast_data.append(sma_fast.update(dt[-1], bid))
-            sma_ff_data.append(sma_ff.update(dt[-1],bid))
-            bollinger_data.append(bollinger.update(dt[-1], bid))            
-            rsi_data.append(rsi.update(secm, bid))
-            rsi_avg_data.append(rsi_avg.update(dt[-1], rsi_data[-1]))
-            
-            
-            if pipperseclaststamp + 1 <= secm:
-                if pipperseclastprice != None:                    
-                    pricediff = bollinger_data[-1][0] - pipperseclastprice 
-                    duration = (secm - pipperseclaststamp)
-                    if duration != 0:
-                        pippersec = pricediff/duration
-                        pippersec_t_v.append((secm, pippersec))
-                pipperseclaststamp = secm
-                pipperseclastprice = bollinger_data[-1][0]
+         
+        """
+        strategy:
+        check for peaks with bollinger 
+        wait for extrema of price 
+        check rsi to be significant and put or call
                 
-             
-            """
-            strategy:
-            check for peaks with bollinger 
-            wait for extrema of price 
-            check rsi to be significant and put or call
-                    
-            """
-            ########################################
-            ### Strategy
-            #######################################################
-            # performance [218, 7, 129]
-            # balance 1908.0 current portfolio []
-            # total actions 347.0
-            # 
-            # winratio 62.8242074928
-            # 
-            #######################################################
+        """
+        ########################################
+        ### Strategy
+        #######################################################
+        # performance [218, 7, 129]
+        # balance 1908.0 current portfolio []
+        # total actions 347.0
+        # 
+        # winratio 62.8242074928
+        # 
+        #######################################################
+        
+        # begin test closing outside bollinger
+        if len(x) > 2 and time.gmtime(x[-2]).tm_min < time.gmtime(x[-1]).tm_min:  # test only on new candle stick
+            if not armed and (ybid[-2] > bollinger_data[-2][1] or ybid[-2] < bollinger_data[-2][2]):
+                armed = 1
+                esxtrema = sma_ff_data[-1]
+                marker_data.append(((datetimemodule.datetime.fromtimestamp(secm)),(bid),'c*'))
+                isup = True if ybid[-1] > bollinger_data[-1][0] else False  
             
-            # begin test closing outside bollinger
-            if len(x) > 2 and time.gmtime(x[-2]).tm_min < time.gmtime(x[-1]).tm_min:  # test only on new candle stick
-                if not armed and (ybid[-2] > bollinger_data[-2][1] or ybid[-2] < bollinger_data[-2][2]):
-                    armed = 1
+        if armed == 1:
+            # crawl on price until momentum turns,
+            if isup:  # find max low 
+                if esxtrema <= sma_ff_data[-1]:
                     esxtrema = sma_ff_data[-1]
-                    marker_data.append(((datetimemodule.datetime.fromtimestamp(secm)),(bid),'c*'))
-                    isup = True if ybid[-1] > bollinger_data[-1][0] else False  
+                elif esxtrema > sma_ff_data[-1]:
+                    # found peak maxima of price
+                    if  rsi_avg_data[-1] > 0.70:
+                        esxtrema = 0  # next stage
+                        armed = 3
+                        expire = secm+60
+                        print "sell " ,bid , bollinger_data[1][-1], rsi_avg_data[-1]
+                        marker_data.append((
+                                            (
+                                                datetimemodule.datetime.fromtimestamp(secm),
+                                                datetimemodule.datetime.fromtimestamp(secm+60),
+                                             ),
+                                            (bid,bid),
+                                            'v-'))
+                        feedofcsv.Option(secm,bid,60,accdata, up=False)
+                    else:
+                        #print "rsi not strong enough"
+                        #marker_data.append(((secm),(bid),'kx'))
+                        armed = 0
+                        esxtrema = 0   
+            if not isup:
+                if esxtrema >= sma_ff_data[-1]:
+                    esxtrema = sma_ff_data[-1]
+                elif esxtrema < sma_ff_data[-1]:   #Todo: make a more sensable stop of interest 
+                    # found peak maxima of price
+                    if rsi_avg_data[-1] < 0.30:
+                        esxtrema = 0  # next stage
+                        armed = 3
+                        expire = secm+60
+                        print "buy " ,bid , bollinger_data[1][-1], rsi_avg_data[-1]
+                        marker_data.append(((
+                                                datetimemodule.datetime.fromtimestamp(secm),
+                                                datetimemodule.datetime.fromtimestamp(secm+60),
+                                             ),
+                                            (bid,bid),
+                                            '^-'))
+                        feedofcsv.Option(secm,bid,60,accdata, up=True)  
+                    else:
+                        #print "rsi not strong enough"
+                        #marker_data.append(((secm),(bid),'kx'))
+                        armed = 0
+                        esxtrema = 0
+        if armed == 3:
+            if secm >= expire:
+                armed = 0
                 
-            if armed == 1:
-                # crawl on price until momentum turns,
-                if isup:  # find max low 
-                    if esxtrema <= sma_ff_data[-1]:
-                        esxtrema = sma_ff_data[-1]
-                    elif esxtrema > sma_ff_data[-1]:
-                        # found peak maxima of price
-                        if  rsi_avg_data[-1] > 0.74:
-                            esxtrema = 0  # next stage
-                            armed = 3
-                            expire = secm+60
-                            print "sell " ,bid , bollinger_data[1][-1], rsi_avg_data[-1]
-                            marker_data.append((
-                                                (
-                                                    datetimemodule.datetime.fromtimestamp(secm),
-                                                    datetimemodule.datetime.fromtimestamp(secm+60),
-                                                 ),
-                                                (bid,bid),
-                                                'v-'))
-                            feedofcsv.Option(secm,bid,60,accdata, up=False)
-                        else:
-                            #print "rsi not strong enough"
-                            #marker_data.append(((secm),(bid),'kx'))
-                            armed = 0
-                            esxtrema = 0   
-                if not isup:
-                    if esxtrema >= sma_ff_data[-1]:
-                        esxtrema = sma_ff_data[-1]
-                    elif esxtrema < sma_ff_data[-1]:   #Todo: make a more sensable stop of interest 
-                        # found peak maxima of price
-                        if rsi_avg_data[-1] < 0.26:
-                            esxtrema = 0  # next stage
-                            armed = 3
-                            expire = secm+60
-                            print "buy " ,bid , bollinger_data[1][-1], rsi_avg_data[-1]
-                            marker_data.append(((
-                                                    datetimemodule.datetime.fromtimestamp(secm),
-                                                    datetimemodule.datetime.fromtimestamp(secm+60),
-                                                 ),
-                                                (bid,bid),
-                                                '^-'))
-                            feedofcsv.Option(secm,bid,60,accdata, up=True)  
-                        else:
-                            #print "rsi not strong enough"
-                            #marker_data.append(((secm),(bid),'kx'))
-                            armed = 0
-                            esxtrema = 0
-            if armed == 3:
-                if secm >= expire:
-                    armed = 0
-                    
-            feedofcsv.Option.update(secm,accdata,bid)    
-                    
-            
-            ### end strategy
-            ####################################
-            
-        wins = feedofcsv.Option.performance[0]
-        losses = float(feedofcsv.Option.performance[2])
-        clearedtotal = wins+losses
+        feedofcsv.Option.update(secm,accdata,bid)    
+                
         
-        print "#######################################################"
-        print "# performance", feedofcsv.Option.performance
-        print "# balance", accdata.balance, 
-        print "current portfolio" , accdata.portfolio
-        print "# total actions"  ,   clearedtotal
-        if clearedtotal: #prevent div by zero
-            print "# "
-            print "# winratio", wins/clearedtotal * 100.
-            print "# "
-            print "#######################################################"        
+        ### end strategy
+        ####################################
+        
+    wins = feedofcsv.Option.performance[0]
+    losses = float(feedofcsv.Option.performance[2])
+    clearedtotal = wins+losses
+    
+    print "#######################################################"
+    print "# performance", feedofcsv.Option.performance
+    print "# balance", accdata.balance, 
+    print "current portfolio" , accdata.portfolio
+    print "# total actions"  ,   clearedtotal
+    if clearedtotal: #prevent div by zero
+        print "# "
+        print "# winratio", wins/clearedtotal * 100.
+        print "# "
+        print "#######################################################"        
 
-        cPickle.dump(
-                (
-                    x,
-                    ybid,
-                    yask,
-                    sma_fast_data,
-                    sma_ff_data,
-                    bollinger_data,
-                    marker_data,
-                    rsi_data,
-                    rsi_avg_data,
-                    pippersec_t_v,
-                    accdata
-                ), pfile)
-    pfile.close()
-    print "done saving cache now start plotting"
-   
-   
     bmean, bupper, blower = zip(*bollinger_data)
     
     
